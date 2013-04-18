a2b00f5c10f3267e9cf2041b9058c7e4aedb701d
Micah Cooper <mrmicahcooper@gmail.com>
2013-04-18 18:42:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treefinger::Application.config.secret_key_base = 'ed52d21b39bac136457959f6b7f2d765b9d76b0537ca7d079da0d73353dbef02d94a212b51712d75933965ef5bd04d0c0b8530cae5d7891e6a9b9d9f34c8d092'
