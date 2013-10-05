c45e29abcdbf80c03c50f217685a916d920477c5
Hashrocket Workstation <dev@hashrocket.com>
2013-10-05 11:06:02 -0400

    Organize javascript functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,84 +1,8 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require sugar
 //= require_tree .
 //= require codemirror
 //= require codemirror/keymaps/vim
-
-var dashboard = {
-
-	load_code_mirror: function(){
-		dashboard.editor = CodeMirror(document.getElementById('editor_textarea'), {
-			lineNumbers: true,
-			keyMap: 'vim',
-			indentWithTabs: true,
-			lineWrapping: true,
-			height: '900px',
-			tabSize: 2,
-		});
-	}
-
-};
-
-$(function(){
-	dashboard.load_code_mirror();
-});
-
-window.App = angular.module("app", ["ngResource"]);
-
-App.config(['$httpProvider', function($httpProvider) {
-	$httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr("content");
-	$httpProvider.defaults.headers.common['Accept'] = "application/json";
-}]);
-
-App.controller("Project", function($scope, $http) {
-
-	$scope.notes = [];
-	$scope.noteAreVisible = false;
-
-	$http.get('/projects/'+project_id+'/tasks').success(function(tasks){
-		$scope.tasks = tasks
-	});
-
-	$scope.showNotes = function(task){
-		var promise;
-		$scope.noteAreVisible = true;
-		$scope.tasks.forEach(function(task) { task.noted = false; });
-		task.noted = true
-		promise = $http.get('/tasks/'+task.id+'/notes');
-		promise.success(function(notes) {
-			$scope.notes = notes;
-		});
-	};
-
-	$scope.addNote = function() {
-		var currentTask = $scope.tasks.find(function(task) {
-			return task.noted == true
-		});
-
-		$http.post(
-			'/tasks/'+currentTask.id+'/notes', {
-				note: { body: $scope.noteText}
-			}
-		).success(function(){
-			$scope.notes.push({
-				body: $scope.noteText,
-				username: current_username
-			});
-			$scope.noteText = ""
-		});
-	};
-
-});
+//= require project
+//= require treefinger
---
+++ app/assets/javascripts/project.js
@@ -0,0 +1,37 @@
+var Project = {
+
+	load_code_mirror: function(){
+		Project.editor = CodeMirror(document.getElementById('editor_textarea'), {
+			lineNumbers: true,
+			keyMap: 'vim',
+			indentWithTabs: true,
+			lineWrapping: true,
+			height: '900px',
+			tabSize: 2,
+		});
+	},
+
+	parse_task_text: function(){
+		taskText = this.editor.getValue();
+
+		taskStrings = taskText.split(/(?=\n\w)/)
+
+		Project.tasks = taskStrings.map(function(taskString){
+			console.log(new Task(taskString));
+
+		})
+
+	}
+};
+
+var Task = function(string){
+	this.taskString = string;
+
+	this.title = function(){
+		return this.taskString.match(/^.+/);
+	}
+};
+
+$(function(){
+	Project.load_code_mirror();
+});
